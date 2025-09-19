0858750497bf688a92333c4aa8629fd4840a6cec
deepvelop99 <101788136+deepvelop99@users.noreply.github.com>
2025-09-19 12:32:16 +0900

    Add files via upload

---
+++ k_nn.py
@@ -0,0 +1,63 @@
+from builtins import range
+from builtins import object
+import numpy as np
+
+
+class KNearestNeighbor(object):
+    """ a kNN classifier with L2 distance """
+
+    def __init__(self):
+        pass
+
+    def train(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X, k=1, num_loops=0):
+        if num_loops == 0:
+            dists = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dists = self.compute_distances_one_loop(X)
+        elif num_loops == 2:
+            dists = self.compute_distances_two_loops(X)
+        else:
+            raise ValueError("Invalid value %d for num_loops" % num_loops)
+
+        return self.predict_labels(dists, k=k)
+
+    # Assignment 1
+    def compute_distances_two_loops(self, X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in range(num_test):
+            for j in range(num_train):
+                # 작성하시요
+        return dists
+    
+    # Assignment 2
+    def compute_distances_one_loop(self, X):        
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in range(num_test):
+            # 작성하시오
+        return dists
+
+    # Assignment 3
+    def compute_distances_no_loops(self, X):
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        # Note: (a - b)^2 = -2ab + a^2 + b^2
+        # 작성하시오
+        return dists
+
+    def predict_labels(self, dists, k=1):
+        num_test = dists.shape[0]
+        y_pred = np.zeros(num_test)
+        for i in range(num_test):
+            closest_y = []
+            closest_y = self.y_train[dists[i].argsort()[:k]]
+            y_pred[i] = np.argmax(np.bincount(closest_y))
+        return y_pred
